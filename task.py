f059e93b2e881c5eab26d0f8be34f4f117e52fce
repl.it user <replituser@example.com>
2020-11-09 15:08:46 +0000

    first commit

--- task.py
+++ task.py
@@ -28,3 +28,50 @@
 #
 #Delete these comments before commit!
 #Good luck.
+
+from dataclasses import dataclass
+import datetime
+
+
+@dataclass
+class Grade:
+  grade: float
+  course: str
+  grade_date: datetime
+
+  def __init__(self, grade, course, grade_date):
+    self.grade = grade
+    self.course = course
+    self.grade_date = grade_date
+
+# dont make it dataclass if it is hard for you, make normal class
+
+class Student:
+  #okay, thanks
+
+  def __init__(self, name, surname):
+    self.name = name
+    self.surname = surname
+    self.grades = list()
+
+  def new_grade(self, course, grade, grade_date):
+    new_grade = Grade(grade, course, grade_date)
+    self.grades.append(new_grade)
+
+  def average_course_grades(self, course):
+    #no time to make it for special one course
+    average = 0.0
+    for i in self.grades:
+      average += self.grades[i].grade
+    return average/len(self.grades)
+
+
+if __name__ == "__main__":
+
+  student = Student("Jan", "Kowalski")
+  today_date = datetime.datetime(2020, 11, 9)
+  student.new_grade("PitE", 3.0, today_date)
+  print("New grade {} for student {} {} at {}".format(3.0, student.name, student.surname, today_date.strftime("%x")))
+
+  print("Student average grade: {} ".format(student.average_course_grades))
+
